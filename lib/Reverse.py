7ce8396b8c75dc069f3aa88f9bfc75a95252f76e
cdchrisdog1 <123763180+cdchrisdog1@users.noreply.github.com>
2023-12-01 15:34:39 -0700

    Update Reverse.py

--- lib/Reverse.py
+++ lib/Reverse.py
@@ -1,23 +1,26 @@
-
-import RPi.GPIO as GPIO
 import sys
+import RPi.GPIO as GPIO
+import time
 
-# Set the pin numbering mode to BCM
 GPIO.setmode(GPIO.BCM)
 
 # Set up pin 26 as an output pin
-GPIO.setup(37, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 
 # Get the command from the terminal
 command = sys.argv[1]
 
 # Check the command and set the pin state accordingly
 if command == "high":
-    GPIO.output(37, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     print("Pin 26 set to HIGH")
+    time.sleep(1)  # wait for 1 second
+    GPIO.output(26, GPIO.LOW)  # reset the pin
 elif command == "low":
-    GPIO.output(37, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     print("Pin 26 set to LOW")
+    time.sleep(1)  # wait for 1 second
+    GPIO.output(26, GPIO.HIGH)  # reset the pin
 else:
     print("Invalid command. Please use 'high' or 'low'.")
 
