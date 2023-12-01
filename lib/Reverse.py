c959b5edb19a3b92a2976c652b6b3520f7724308
cdchrisdog1 <123763180+cdchrisdog1@users.noreply.github.com>
2023-12-01 16:19:02 -0700

    Update Reverse.py

--- lib/Reverse.py
+++ lib/Reverse.py
@@ -1,28 +1,27 @@
-import sys
+from pynput import keyboard
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-
-# Set up pin 26 as an output pin
 GPIO.setup(26, GPIO.OUT)
 
-# Get the command from the terminal
-command = sys.argv[1]
+command = 'low'
+def on_press(key):
+    global command
+    if key == keyboard.Key.up:
+        command = 'high'
+    elif key == keyboard.Key.down:
+        command = 'low'
 
-# Check the command and set the pin state accordingly
-if command == "high":
-    GPIO.output(26, GPIO.HIGH)
-    print("Pin 26 set to HIGH")
-    time.sleep(1)  # wait for 1 second
-    GPIO.output(26, GPIO.LOW)  # reset the pin
-elif command == "low":
-    GPIO.output(26, GPIO.LOW)
-    print("Pin 26 set to LOW")
-    time.sleep(1)  # wait for 1 second
-    GPIO.output(26, GPIO.HIGH)  # reset the pin
-else:
-    print("Invalid command. Please use 'high' or 'low'.")
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
 
-# Clean up GPIO settings
-GPIO.cleanup()
+try:
+    while True:
+        if command == 'high':
+            GPIO.output(26, GPIO.HIGH)
+        else:
+            GPIO.output(26, GPIO.LOW)
+        time.sleep(1)
+finally:
+    GPIO.cleanup()
