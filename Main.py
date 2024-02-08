ff91fb6ba7b987e2178080e1f30d25b7ab214bad
spenc3rb <beersc@colostate.edu>
2024-02-08 13:44:55 -0700

    Changes to stereothread.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import sys
 from PySide6.QtGui import QGuiApplication, QFontDatabase
 from PySide6.QtQml import QQmlApplicationEngine
-import lib.DashboardController
+import lib.controllers.DashboardController
 import lib.APDView
 	
 app = QGuiApplication([])
--- lib/StereoThread.py
+++ lib/StereoThread.py
@@ -7,6 +7,8 @@ from multiprocessing.shared_memory import SharedMemory
 import numpy as np
 import time
 import cv2
+import depthai as dai
+import sys
 
 class VideoThread(QThread):
 	frameChanged = Signal(bool)
@@ -15,39 +17,75 @@ class VideoThread(QThread):
 	def __init__(self):
 		super().__init__()
 		self._run_flag = True
+		self.nnPath = 'mobilenet-ssd_openvino_2021.4_6shave.blob'
+		self.labelMap = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
+		            "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+		self.includedLabels = ["person", "car", "motorbike", "bicycle", "bus", "aeroplane", "background", "train"]
+		self.create_pipeline()
+		self.disparityMultiplier = 255 / self.depth.initialConfig.getMaxDisparity()
+	
+	def create_pipeline(self):
+		# Create pipeline
+		self.pipeline = dai.Pipeline()
+		
+		# Define sources and outputs
+		self.camRgb = self.pipeline.create(dai.node.ColorCamera)
+		self.videoEncoder = self.pipeline.create(dai.node.VideoEncoder)
+		self.monoRight = self.pipeline.create(dai.node.MonoCamera)
+		self.monoLeft = self.pipeline.create(dai.node.MonoCamera)
+		self.depth = self.pipeline.create(dai.node.StereoDepth)
+		self.manip = self.pipeline.create(dai.node.ImageManip)
+		self.nn = self.pipeline.create(dai.node.MobileNetDetectionNetwork)
+		
+		self.videoOut = self.pipeline.create(dai.node.XLinkOut)
+		self.xoutRight = self.pipeline.create(dai.node.XLinkOut)
+		self.disparityOut = self.pipeline.create(dai.node.XLinkOut)
+		self.manipOut = self.pipeline.create(dai.node.XLinkOut)
+		self.nnOut = self.pipeline.create(dai.node.XLinkOut)
+		
+		self.videoOut.setStreamName('h265')
+		self.xoutRight.setStreamName('right')
+		self.disparityOut.setStreamName('disparity')
+		self.manipOut.setStreamName('manip')
+		self.nnOut.setStreamName('nn')
+		
+		# Properties
+		self.camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
+		self.camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+		self.monoRight.setCamera("right")
+		self.monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+		self.monoLeft.setCamera("left")
+		self.monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+		self.videoEncoder.setDefaultProfilePreset(30, dai.VideoEncoderProperties.Profile.H265_MAIN)
+		
+		self.depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+		self.depth.setRectifyEdgeFillColor(0)
 
+		self.nn.setConfidenceThreshold(0.5)
+		self.nn.setBlobPath(self.nnPath)
+		self.nn.setNumInferenceThreads(2)
+		self.nn.input.setBlocking(False)
 
-	def run(self):
-		try:
-			#create a shared memory for reading the frame shape
-			frame_shape_shm = SharedMemory(name="frame_shape")
-			frame_shape = np.ndarray([3], buffer=frame_shape_shm.buf, dtype='i4')
+		self.manip.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
+		self.manip.initialConfig.setResize(300, 300)
 
-			#create the shared memory for the frame buffer
-			frame_buffer_shm = SharedMemory(name="frame_buffer")
-
-			#create the framebuffer using the shm's memory
-			frame_buffer = np.ndarray(frame_shape, buffer=frame_buffer_shm.buf, dtype='u1')
-
-			# Retrieve new frames from frame_buffer and send them off to App
-			while self._run_flag:
-				#qframe = self.convert_cv_qt(frame_buffer)
-				#self.frameChanged.emit(qframe)
-				self.qframe = self.convert_cv_qt(frame_buffer)
-				self.frameChanged.emit(self.qframe)
-				time.sleep(0.02)
-				#print("in run")
-				#self.frameChanged.emit(True)
-		except:
-			print("ERROR: Unable to connect to APD via shared memory. Check that APD's detect.py is running.")
-
-
+		self.camRgb.video.link(self.videoEncoder.input)
+		self.videoEncoder.bitstream.link(self.videoOut.input)
+		self.monoRight.out.link(self.xoutRight.input)
+		self.monoRight.out.link(self.depth.right)
+		self.monoLeft.out.link(self.depth.left)
+		self.depth.disparity.link(self.disparityOut.input)
+		self.depth.rectifiedRight.link(self.manip.inputImage)
+		self.manip.out.link(self.nn.input)
+		self.manip.out.link(self.manipOut.input)
+		self.nn.out.link(self.nnOut.input)
+	
 	def stop(self):
 		"""Sets run flag to False and waits for thread to finish"""
 		self._run_flag = False
 		self.wait()
-		
-		
+					
+					
 	def get_frame(self):
 		return self.qframe
 
@@ -60,3 +98,13 @@ class VideoThread(QThread):
 		convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
 		p = convert_to_Qt_format.scaled(640, 480, Qt.KeepAspectRatio)
 		return p
+
+	def run(self):
+		try:
+			with dai.Device(self.pipeline) as device:
+				# Output queues will be used to get the grayscale frames from the outputs defined above
+				qRight = device.getOutputQueue(name="right", maxSize=4, blocking=False)
+				qDisparity = device.getOutputQueue(name="disparity", maxSize=4, blocking=False)
+				qManip = device.getOut
+
+				
